17ed5622177829283c571aae02a6319e470b31af
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-13 12:25:02 -0300

    Formatação de contrato

--- servicos/webscrapingbase.py
+++ servicos/webscrapingbase.py
@@ -2,4 +2,11 @@ from abc import abstractmethod, ABC
 
 
 class WebScapingBase(ABC):
-    pass
+
+    @abstractmethod
+    def conectar_site(self):
+        pass
+
+    @abstractmethod
+    def obter_dados(self):
+        pass
