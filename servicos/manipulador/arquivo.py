84ddf4c3c4739ce69c267b1360bb7793709febf9
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-20 13:49:52 -0300

    tipagem arquivo

--- servicos/manipulador/arquivo.py
+++ servicos/manipulador/arquivo.py
@@ -8,7 +8,7 @@ from models.noticia import Noticia
 class Arquivo(ABC):
     def __init__(self, nome_arquivo: str):
         self._caminho_raiz = os.getcwd()
-        self._nome_arquivo = Optional[nome_arquivo] = None
+        self._nome_arquivo = None
         self.__noticia: Optional[Noticia] = None
 
     @property
