e6917330250c5bc8b1526fc707aa49b4cf5910f5
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-20 22:25:33 -0300

    interface de conexão da api

---
+++ servicos/s_api/inoticia_api.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod, ABC
+from models.noticia import Noticia
+
+
+class INoticiaApi(ABC):
+
+    @abstractmethod
+    def salvar_dados(self, noticia: Noticia):
+        pass
+
+    @abstractmethod
+    def consultar_dados_id(self, id_noticia) -> Noticia:
+        pass
