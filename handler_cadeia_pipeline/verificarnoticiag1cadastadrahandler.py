80f98a0a15463e683bdcd161211c74e8ab485779
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-22 22:25:33 -0300

    Classe para verificar noticia cadastrada

--- handler_cadeia_pipeline/verificarnoticiag1cadastadrahandler.py
+++ handler_cadeia_pipeline/verificarnoticiag1cadastadrahandler.py
@@ -1,3 +1,8 @@
+import hashlib
+
+from click import Tuple
+
+from context.pipeline_context import PipelineContext
 from handler_cadeia_pipeline.handler import Handler
 from servicos.s_api.inoticia_api import INoticiaApi
 
@@ -5,4 +10,15 @@ from servicos.s_api.inoticia_api import INoticiaApi
 class VerificarNoticiaCadastradaHandler(Handler):
     def __init__(self, api_noticia: INoticiaApi):
         super().__init__()
-        self._api_noticia
+        self._api_noticia = api_noticia
+
+    def executar_processo(self, context: PipelineContext) -> bool:
+        try:
+            for url in context.url_noticia_g1:
+                id_noticia = hashlib.md5(url.encode('utf-8')).hexdigest()
+                id_noticia_api = self._api_noticia.consultar_dados_id(id_noticia=id_noticia)
+                if not isinstance(id_noticia_api, Tuple):
+                    context.url_noticia_nao_cadastrada.append(url)
+            return True
+        except Exception:
+            return False
