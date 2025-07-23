a3d1e6aa9c7681d2637a41a5623d73c301dc3bb2
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-22 22:10:53 -0300

    Setando lista vazias

--- context/pipeline_context.py
+++ context/pipeline_context.py
@@ -6,10 +6,10 @@ R2 = TypeVar('R2')
 R3 = TypeVar('R3')
 
 
-class PipelineContext(Generic[R1, R2]):
+class PipelineContext(Generic[R1]):
 
     def __init__(self, api: INoticiaApi):
         self.api = api
         self.rss: Optional[R1] = None
-        self.noticia: Optional[R2] = None
-        self.url_noticia_g1: Optional[List[str]] = []
+        self.url_noticia_nao_cadastrada: List[str] = []
+        self.url_noticia_g1: List[str] = []
